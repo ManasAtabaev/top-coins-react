9961f701cca59fa490730ed276c3c862e4d556a3
ManasAtabaev <manas.atabaev@gmail.com>
2021-11-13 17:20:25 +0800

    React Tailwind base project

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 
+import HomePage from './pages/home/HomePage';
+import LiquidityPage from './pages/liquidity/LiquidityPage';
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/liquidity" element={<LiquidityPage />} />
+        </Routes>
+    );
 }
 
 export default App;
